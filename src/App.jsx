97a4bbd792b2d835d5c1204d8696feec4245df25
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-08 09:10:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Login from './components/pages/Login'
 import Manageusers from './components/pages/Manage users'
 import Profile from './components/pages/Profile'
 import Register from './components/pages/Register'
+import Navbar from './components/Navbar'
+import Messages from './components/Messages'
 
 function App() {
   return (
     <>
+      <Navbar />
+      <Messages />
       <Home />
       <Register />
       <Login />
       <Profile />
-      <Manageusers />
+      <Messages />
     </>
   )
 }
