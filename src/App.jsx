d036c4d16fce4977e71768006788d0377712f1b5
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-09 07:28:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
+import React from 'react'
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
 import Manageusers from './components/pages/Manage users'
 import Profile from './components/pages/Profile'
 import Register from './components/pages/Register'
-import Navbar from './components/Navbar'
 import Messages from './components/Messages'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Messages />
-      <Home />
-      <Register />
-      <Login />
-      <Profile />
-      <Messages />
+      <Router>
+        <Navbar />
+        <Messages />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/manageusers' element={<Manageusers />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
