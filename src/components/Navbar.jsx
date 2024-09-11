30ae76534ea2047ec38c5bfc6b4cc1813e68b2fd
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-11 15:47:11 +0100

    Add link to pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,20 +44,21 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Navbar.css'
+// import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <Navbar expand="md" className="navbar bg-body-tertiary mt-0 mx-2 p-0 sticky-top rounded border border-danger">
+    <Navbar expand="md" className="navbar bg-body-tertiary bg-dark-subtle mt-0 mx-0 p-0 sticky-top rounded">
       <Container>
-        <img src="/public/favicon.ico" alt="logo" width="30" height="24" className="d-inline-block align-text-top m-1 border border-warning" />
-        <a className="badge text-bg-light text-wrap fst-italic p-2 border border-warning" >RBAC</a>
+        <img src="/public/favicon.ico" alt="logo" width="30" height="24" className="d-inline-block align-text-top m-1 justifyborder border-warning" />
+        <a className="badge text-bg-light text-wrap fst-italic p-1 border border-info" >RBAC</a>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="p-2 border border-warning justify-content-end">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Register</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="p-0">
+            <Nav.Link as={Link} to="#home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
             <Nav.Link href="#link">Logout</Nav.Link>
             <Nav.Link href="#link">Manage users</Nav.Link>
             <Nav.Link href="#link">Profile</Nav.Link>
