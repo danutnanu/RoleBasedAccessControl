740826dca638140f597f246996229ba8d0980533
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-11 15:45:27 +0100

    Add Add default content for the homepage

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,35 +1,57 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function Home() {
   return (
     <>
-      <div className='text-center m-2 pb-5 border border-danger rounded'>
-        <p className='h1'>Home Page</p>
-        <p className='h3'>Designed by: Danut Nanu</p>
-      </div>
-      <div className='text-center m-2 pb-5 border border-danger rounded'>
-        <p className='h1'>Home Page</p>
-        <p className='h3'>Designed by: Danut Nanu</p>
-      </div>
-      <div className='text-center m-2 pb-5 border border-danger rounded'>
-        <p className='h1'>Home Page</p>
-        <p className='h3'>Designed by: Danut Nanu</p>
-      </div>
-      <div className='text-center m-2 pb-5 border border-danger rounded'>
-        <p className='h1'>Home Page</p>
-        <p className='h3'>Designed by: Danut Nanu</p>
-      </div>
-      <div className='text-center m-2 pb-5 border border-danger rounded'>
-        <p className='h1'>Home Page</p>
-        <p className='h3'>Designed by: Danut Nanu</p>
-      </div>
-      <div className='text-center m-2 pb-5 border border-danger rounded'>
-        <p className='h1'>Home Page</p>
-        <p className='h3'>Designed by: Danut Nanu</p>
-      </div>
-      <div className='text-center m-2 pb-5 border border-danger rounded'>
-        <p className='h1'>Home Page</p>
-        <p className='h3'>Designed by: Danut Nanu</p>
+      <div className='px-5 pb-5 bg-info-subtle'>
+        <p className='h4 fw-bold text-center mb-0 rounded'>Role Based Access Control</p>
+        <div className='text-start mx-5 p-5 pt-2 border border-dark rounded'>
+          <p className='fw-bold text-center text-primary mb-4 mt-0'>HOME PAGE: "This content is visible to all, without any restrictions. The page is public and can be viewed without logging in."</p>
+          <p className='h5 fw-bold mb-4'>RBAC with React</p>
+          <p className='h6 fw-bold'>Project Overview</p>
+          <p className='text-start'>This project implements a Role-Based Access Control (RBAC) system using React. The system allows for managing user roles and controlling access to various parts of the application based on their assigned permissions.</p>
+          <p className='h6 fw-bold'>Technologies Used:</p>
+          <ListGroup as="ol" numbered>
+            <ListGroup.Item
+              as="li"
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="ms-1 me-auto">
+                <div className="fw-bold">React:</div>
+                Frontend framework.
+              </div>
+
+            </ListGroup.Item>
+            <ListGroup.Item
+              as="li"
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="ms-1 me-auto">
+                <div className="fw-bold">React Router:</div>
+                For navigation and route protection.
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item
+              as="li"
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="ms-1 me-auto">
+                <div className="fw-bold">JavaScript (ES6+):</div>
+                Core programming language for the project.
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item
+              as="li"
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="ms-1 me-auto">
+                <div className="fw-bold">Optional Libraries:</div>
+                Bootstrap.
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </div>
       </div>
     </>
   );
