5fbe3c37a4209e2b80b9bb43b27dc0f9b33cd6bb
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-12 14:38:29 +0100

    Import Navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import './Navbar.css'
+import './Navbar.css'
 import { Link } from 'react-router-dom';
 
 function Navigation() {
