26ad32cf96086d32211e8706a4e6929ee3ebaac3
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-17 10:17:22 +0100

    Add messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -4,7 +4,8 @@ function Messages() {
   return (
     <>
    <div className='my-1 mt-4 text-center'>
-    <p className='h6'>Messages page</p>
+    <p className='h6'>Registered successfully, you can now login</p>
+    <p>You are logged in with a user,moderator or admin ID</p>
    </div>
     </>
   );
