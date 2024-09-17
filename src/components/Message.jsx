9a8a0700c637e14522cde0a855d5be268adeec3e
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-17 11:37:42 +0100

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,7 +5,7 @@ function Messages() {
     <>
    <div className='my-1 mt-4 text-center'>
     <p className='h6'>Registered successfully, you can now login</p>
-    <p>You are logged in with a user,moderator or admin ID</p>
+    <p>You are logged in with a user, moderator or admin ID</p>
    </div>
     </>
   );
