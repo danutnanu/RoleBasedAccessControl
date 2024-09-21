f92cfe3f9d6a9affdfd37a13c629d9e9e830442a
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-21 08:49:06 +0100

    Fix Navbar to the top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <Navbar expand="md" className="navbar bg-body-tertiary bg-dark-subtle mt-0 mx-0 p-0 sticky-top rounded">
+    <Navbar expand="md" className="navbar bg-body-tertiary bg-dark-subtle mt-0 mx-0 p-0 fixed-top rounded">
       <Container>
         <img src="/public/favicon.ico" alt="logo" width="30" height="24" className="d-inline-block align-text-top m-1 justifyborder border-warning" />
         <a className="badge text-bg-light text-wrap fst-italic p-1 border border-info" >RBAC</a>
