695e4e24ceae0c223102956eeb0147a59e33038b
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-09-21 09:08:57 +0100

    Fix message page

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 function Messages() {
   return (
     <>
-   <div className='my-1 mt-4 text-center'>
-    <p className='h6'>Registered successfully, you can now login</p>
-    <p>You are logged in with a user, moderator or admin ID</p>
-   </div>
+      <div className='my-1 mt-5 text-center'>
+        <p className='h6'>Registered successfully, you can now login</p>
+      </div>
     </>
   );
 }
