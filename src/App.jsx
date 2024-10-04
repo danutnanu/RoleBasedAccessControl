0918eb1c3b47ffc3653bccf02a2fbc1fd36d3c53
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-10-04 17:29:30 +0100

    Update App layout and integrate Message component
    - Add MessageProvider wrapper
    - Position Message component below Navbar
    - Improve overall app structure with app-container and content-container

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react';
-import Navbar from './components/Navbar'
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Message, { MessageProvider } from './components/Message'
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
 import ManageUsers from './components/pages/ManageUsers'
 import Profile from './components/pages/Profile'
 import Register from './components/pages/Register'
-import Message from './components/Message'
 import { initializeUsers } from './utils/userStorage';
-import { MessageProvider } from './context/MessageContext';
+import './App.css'
 
 function App() {
   useEffect(() => {
@@ -19,15 +18,21 @@ function App() {
   return (
     <MessageProvider>
       <Router>
-        <Navbar />
-        <Message />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/manageusers' element={<ManageUsers />} />
-        </Routes>
+        <div className="app-container">
+          <Navbar />
+          <div className="message-container">
+            <Message />
+          </div>
+          <div className="content-container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/manageusers' element={<ManageUsers />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </MessageProvider>
   );
