9e27da59bf9439bbeb12170a5f9b799981bcbb53
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-10-15 16:11:41 +0100

    Update Bootstrap classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,11 @@ function Navigation() {
 
   return (
     <>
-      <Navbar expanded={expanded} onToggle={() => setExpanded(!expanded)} expand="md" className="navbar fixed-top bg-body-tertiary bg-dark-subtle mt-0 mx-0 p-0 rounded">
+      <Navbar expanded={expanded} onToggle={() => setExpanded(!expanded)} expand="md" className="navbar fixed-top bg-body-tertiary bg-dark-subtle mt-0 mx-0 p-0">
         <Container>
           <img src="/public/favicon.ico" alt="logo" width="30" height="24" className="d-inline-block align-text-top m-1" />
           <a className="badge text-bg-light text-wrap fst-italic p-1 border border-info" >RBAC</a>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="my-1" onClick={() => setExpanded(!expanded)} />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end text-center me-3">
             <Nav className="p-0">
               <Nav.Link as={Link} to="/" onClick={handleLinkClick}>Home</Nav.Link>
