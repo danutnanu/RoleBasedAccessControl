4602fe8ee723159526a5379076d486c20eaf87b9
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-10-29 07:18:26 +0000

    Implement dynamic content rendering on Home page based on user role

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -16,37 +16,25 @@ function Home() {
             <p className='text-start'>This project implements a Role-Based Access Control (RBAC) system using React. The system allows for managing user roles and controlling access to various parts of the application based on their assigned permissions.</p>
             <p className='h6 fw-bold'>Technologies Used:</p>
             <ListGroup as="ol" numbered>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
+              <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
                 <div className="ms-1 me-auto">
                   <div className="fw-bold">React:</div>
                   Frontend framework.
                 </div>
               </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
+              <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
                 <div className="ms-1 me-auto">
                   <div className="fw-bold">React Router:</div>
                   For navigation and route protection.
                 </div>
               </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
+              <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
                 <div className="ms-1 me-auto">
                   <div className="fw-bold">JavaScript (ES6+):</div>
                   Core programming language for the project.
                 </div>
               </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
+              <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
                 <div className="ms-1 me-auto">
                   <div className="fw-bold">Optional Libraries:</div>
                   Bootstrap.
@@ -72,7 +60,7 @@ function Home() {
             </div>
           </section>
 
-          {(user.role === 'moderator' || user.role === 'admin') && (
+          {(user.role.toLowerCase() === 'moderator' || user.role.toLowerCase() === 'admin') && (
             <section id="moderator">
               <div className='px-5 p-5 bg-warning-subtle'>
                 <div className='text-start mx-5 p-5 pt-2 border border-dark rounded'>
@@ -87,7 +75,7 @@ function Home() {
             </section>
           )}
 
-          {user.role === 'admin' && (
+          {user.role.toLowerCase() === 'admin' && (
             <section id="admin">
               <div className='px-5 p-5 bg-danger-subtle'>
                 <div className='text-start mx-5 p-5 pt-2 border border-dark rounded'>
